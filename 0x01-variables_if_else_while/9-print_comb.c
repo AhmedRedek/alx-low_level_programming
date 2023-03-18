c5755dec660c1fd20ebfe766e0943fb468ee65e4
Ahmed <ahmedkeder91@gmail.com>
2023-03-18 03:17:35 -0700

    Crazy Ascending

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	
+	for(a = 0; a < 10; a++)
+	{
+		putchar(a);
+		if(a < 9)
+		{
+			putchar(', ');
+		}
+	}
+
+	return (0);
+}
