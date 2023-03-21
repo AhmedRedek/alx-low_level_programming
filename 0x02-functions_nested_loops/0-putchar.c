376e365556d84c16b6b1d6ce9338d9c7859736f8
Ahmed <ahmedkeder91@gmail.com>
2023-03-21 12:44:45 -0700

    PUTCHAR

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * Print _putchar with new line
+ *
+ *
+ *
+ *
+ * return 0
+ *
+ *
+ **/
+
+int main()
+{
+	printf("_putchar");
+	printf("\n");
+	return 0;
+}
