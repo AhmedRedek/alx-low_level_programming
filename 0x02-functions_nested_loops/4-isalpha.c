7f63ae2a0a338c4aa36d54f88a7cda40767ed9c5
Ahmed <ahmedkeder91@gmail.com>
2023-03-22 16:06:07 -0700

    Ahmed alpha case

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * _isalpha - a function that checks for alpha character
- * @c: single letter input
- * Return: 1 if int c is lowercase, 0 if otherwise
+ *_isalpha - a function that checks for alpha character
+ *@c: single letter input
+ *Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _isalpha(int c)
 {
-
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 	       return (1);
 	return (0);
-}	
+}
