eaf87cd0ec091971af9cc813b01f2082ea371b24
Ahmed <ahmedkeder91@gmail.com>
2023-03-24 16:50:21 -0700

    0 - 9 except 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers()
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '1'; x <= '9'; x++)
+	{
+		if (x != '2' || x != '4')
+			_putchar(x);
+	}
+	_putchar('\n');
+}
