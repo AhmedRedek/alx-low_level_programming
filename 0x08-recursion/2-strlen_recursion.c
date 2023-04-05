fb98741cafd96df40a3c78a536098589dc63da50
Ahmed <ahmedkeder91@gmail.com>
2023-04-05 11:47:49 -0700

    Billal

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,10 +3,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int counter;
 
-	for (counter = 0; *s != '\n'; )
-		counter ++;
-	_strlen_recursion(s + 1);
-	return (counter);
+	if (*s != '\n')
+		return;
+	return (1 + _strlen_recursion(*s + 1));
 }
