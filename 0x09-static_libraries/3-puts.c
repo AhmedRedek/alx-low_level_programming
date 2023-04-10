7d5a256eb4cd70336bcab0ae4dff755475f0af43
Ahmed <ahmedkeder91@gmail.com>
2023-04-10 12:44:03 -0700

    Hello

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- */
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: the string
+**/
+
 void _puts(char *str)
 {
 	int i;
@@ -12,5 +13,6 @@ void _puts(char *str)
 	{
 		_putchar(str[i]);
 	}
+
 	_putchar('\n');
 }
